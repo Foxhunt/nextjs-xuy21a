7dff2e9be5b9188773b5120907d09318e9a816a6
Foxhunt <marco@mojica.de>
2021-05-27 19:19:22 +0200

    Style Bio

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,18 +1,33 @@
+import styled from "styled-components"
 import { GitHub, Instagram } from 'react-feather';
 
+const Container = styled.div`
+
+`
+
+const Line = styled.p`
+  margin: 4vmin 0px;
+`
+
+const Link = styled.a`
+  margin-right: 5vmin;
+`
+
 export default function Bio(){
-  return <>
-    <p>Marco Mojica</p>
-    <p>
+  return <Container>
+    <Line>
+      Marco Mojica
+    </Line>
+    <Line>
       <a href="mailto:marco@mojica.de">marco@mojica.de</a>
-    </p>
-    <p>
-      <a href="https://github.com/Foxhunt" target="_blank">
-        <GitHub />
-      </a>
-      <a href="https://www.instagram.com/wasfuernname/" target="_blank">
-        <Instagram />
-      </a>
-    </p>
-  </>
+    </Line>
+    <Line>
+      <Link href="https://github.com/Foxhunt" target="_blank">
+        <GitHub width="8vmin" height="8vmin"/>
+      </Link>
+      <Link href="https://www.instagram.com/wasfuernname/" target="_blank">
+        <Instagram width="8vmin" height="8vmin"/>
+      </Link>
+    </Line>
+  </Container>
 }
