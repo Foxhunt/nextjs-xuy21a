8709ebfdaf0a31974fa9a3d80f48416b433875b0
Foxhunt <marco@mojica.de>
2021-05-27 18:40:18 +0200

    Add Projects

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,3 +1,27 @@
+import styled from "styled-components"
+
+const Entry = styled.a`
+  margin: 5px 0px;
+`
+
 export default function Work(){
-  return <></>
+  return <>
+    <p>Projects</p>
+    <Entry
+      href="https://indexed.cloud/">
+      indexed.cloud
+    </Entry>
+    <Entry
+      href="https://github-event-well-foxhunt.vercel.app/">
+      github-event-well
+    </Entry>
+    <Entry
+      href="https://convos.art/">
+      convos.art
+    </Entry>
+    <Entry
+      href="https://dreambook.space/">
+      dreambook.space
+    </Entry>
+  </>
 }
