fe5391eff5b2ac83980b6f8ba57aceb6ab7d89f9
Marco <marco@mojica.de>
2021-06-27 04:51:28 +0200

    Add comments and production SourceMaps

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,15 @@ import { Container, Stack } from '@chakra-ui/react';
 import Bio from "../components/Bio"
 import Work from "../components/Work"
 
+// Hello there ( •̀ ω •́ )y
+// If this “Emoji” bothers you as much as me, welcome.
+// The fact you are reading this lets me know you are of the curious kind.
+// You either cloned the repo, started editing on StackBlitz or just checked out the files on GitHub.
+// This is a personal Blog and portfolio Page.
+// You are welcome to look around and give your thoughts.
+// I hope you find something for your curiosity.
+
+
 export default function Home() {
   return (
     <Container centerContent pt="5" color="black">
