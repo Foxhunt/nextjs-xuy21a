183367ae3845e04b8e20dea34752493b71d966a1
Foxhunt <marco@mojica.de>
2022-01-31 18:33:42 +0100

    Add phone number to Bio

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,38 +1,28 @@
-import { Container, Heading, HStack, Link, Icon } from "@chakra-ui/react";
-import { GitHub, Instagram, Mail } from 'react-feather';
+import { Container, Heading, HStack, Link, Icon } from '@chakra-ui/react';
+import { GitHub, Instagram, Mail, Phone } from 'react-feather';
 
 export default function Bio() {
-  return <Container>
-    <Heading whiteSpace="nowrap">
-      Marco Mojica
-    </Heading>
-    <HStack>
-      <Link
-        aria-label="Mail"
-        href="mailto:marco@mojica.de">
-        <Icon
-          w="7"
-          h="7"
-          as={Mail} />
-      </Link>
-      <Link
-        aria-label="GitHub"
-        isExternal
-        href="https://github.com/Foxhunt">
-        <Icon
-          w="7"
-          h="7"
-          as={GitHub} />
-      </Link>
-      <Link
-        aria-label="Instagram"
-        isExternal
-        href="https://www.instagram.com/wasfuernname/">
-        <Icon
-          w="7"
-          h="7"
-          as={Instagram} />
-      </Link>
-    </HStack>
-  </Container >
+  return (
+    <Container>
+      <Heading whiteSpace="nowrap">Marco Mojica</Heading>
+      <HStack>
+        <Link aria-label="Mail" isExternal href="mailto:marco@mojica.de">
+          <Icon w="7" h="7" as={Mail} />
+        </Link>
+        <Link aria-label="Mail" isExternal href="tel:+4915154750027">
+          <Icon w="7" h="7" as={Phone} />
+        </Link>
+        <Link aria-label="GitHub" isExternal href="https://github.com/Foxhunt">
+          <Icon w="7" h="7" as={GitHub} />
+        </Link>
+        <Link
+          aria-label="Instagram"
+          isExternal
+          href="https://www.instagram.com/wasfuernname/"
+        >
+          <Icon w="7" h="7" as={Instagram} />
+        </Link>
+      </HStack>
+    </Container>
+  );
 }
