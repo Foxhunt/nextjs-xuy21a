04cba3f3ca2d11aed182d521562433b9b9bf7a4d
Foxhunt <marco@mojica.de>
2022-12-26 21:43:29 +0100

    1 file modified

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,42 +1,53 @@
-import { Link, Heading, Container, VStack } from "@chakra-ui/react"
+import { Link, Heading, Container, VStack } from '@chakra-ui/react';
 
 export default function Work() {
-  return <Container>
-    <Heading>Projects</Heading>
-    <VStack
-      spacing="2"
-      fontSize="2xl"
-      align="flex-start">
-      <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://indexed.cloud/">
-        indexed.cloud
-      </Link>
-      <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github-event-well-foxhunt.vercel.app/">
-        github-event-well
-      </Link>
-      <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://convos.art/">
-        convos.art
-      </Link>
-      <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://dreambook.space/">
-        dreambook.space
-      </Link>
-      <Link
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://quorum-sensing-axnuog89l.now.sh/">
-        quorum-sensing
-      </Link>
-    </VStack>
-  </Container>
+  return (
+    <Container>
+      <Heading>Projects</Heading>
+      <VStack spacing="2" fontSize="2xl" align="flex-start">
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://indexed.cloud/"
+        >
+          indexed.cloud
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.welldone24.de/"
+        >
+          welldone24
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github-event-well-foxhunt.vercel.app/"
+        >
+          github-event-well
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://convos.art/"
+        >
+          convos.art
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://dreambook.space/"
+        >
+          dreambook.space
+        </Link>
+        <Link
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://quorum-sensing-axnuog89l.now.sh/"
+        >
+          quorum-sensing
+        </Link>
+      </VStack>
+    </Container>
+  );
 }
