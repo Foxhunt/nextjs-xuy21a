f9b3309beaeae011eef7f3a5af186c4bbc790773
Marco Mojica <marco@mojica.de>
2023-03-18 19:15:08 +0100

    Change bio stlye

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,4 +1,11 @@
-import { Container, Heading, HStack, Link, Icon } from "@chakra-ui/react";
+import {
+  Container,
+  Heading,
+  HStack,
+  Link,
+  Icon,
+  VStack,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { GitHub, Instagram, Mail, Phone } from "react-feather";
@@ -8,39 +15,45 @@ import mmojica from "../public/mmojica.jpg";
 export default function Bio() {
   const router = useRouter();
   return (
-    <Container gap={6} display="flex" flexDir="column">
-      <Heading
-        whiteSpace="nowrap"
-        onContextMenu={(event) => {
-          event.preventDefault();
-          router.push("weeks");
-        }}
-      >
-        Marco Mojica
-      </Heading>
-      <HStack justify={"space-between"}>
-        <Link aria-label="Mail" isExternal href="mailto:marco@mojica.de">
-          <Icon w="7" h="7" as={Mail} />
-        </Link>
-        <Link
-          aria-label={"+4915154750027".split("").join(" ")}
-          isExternal
-          href="tel:+4915154750027"
+    <Container>
+      <VStack gap={3} display="flex" flexDir="column">
+        <Heading
+          whiteSpace="nowrap"
+          onContextMenu={(event) => {
+            event.preventDefault();
+            router.push("weeks");
+          }}
         >
-          <Icon w="7" h="7" as={Phone} />
-        </Link>
-        <Link aria-label="GitHub" isExternal href="https://github.com/Foxhunt">
-          <Icon w="7" h="7" as={GitHub} />
-        </Link>
-        <Link
-          aria-label="Instagram"
-          isExternal
-          href="https://www.instagram.com/wasfuernname/"
-        >
-          <Icon w="7" h="7" as={Instagram} />
-        </Link>
-      </HStack>
-      <Image src={mmojica} alt="Marco Mojica" />
+          Marco Mojica
+        </Heading>
+        <HStack justify={"space-between"}>
+          <Link aria-label="Mail" isExternal href="mailto:marco@mojica.de">
+            <Icon w="7" h="7" as={Mail} />
+          </Link>
+          <Link
+            aria-label={"+4915154750027".split("").join(" ")}
+            isExternal
+            href="tel:+4915154750027"
+          >
+            <Icon w="7" h="7" as={Phone} />
+          </Link>
+          <Link
+            aria-label="GitHub"
+            isExternal
+            href="https://github.com/Foxhunt"
+          >
+            <Icon w="7" h="7" as={GitHub} />
+          </Link>
+          <Link
+            aria-label="Instagram"
+            isExternal
+            href="https://www.instagram.com/wasfuernname/"
+          >
+            <Icon w="7" h="7" as={Instagram} />
+          </Link>
+        </HStack>
+        <Image src={mmojica} alt="Marco Mojica" />
+      </VStack>
     </Container>
   );
 }
